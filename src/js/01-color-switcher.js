2442f302adc051c99410247c9d3e5367e2f71336
Julie Shapovalenko <jools.shapo@gmail.com>
2023-02-27 20:51:39 +0200

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+const PROMPT_DELAY = 1000;
+let intervalId = null;
+
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    refs.body.style.backgroundColor = getRandomHexColor(); 
+
+    if(intervalId) return;
+    
+    intervalId = setInterval(() => {
+        console.log('color')
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, PROMPT_DELAY);
+            
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+} 
+
+function onStopBtnClick() {
+    clearInterval(intervalId);
+    intervalId = 0;
+}

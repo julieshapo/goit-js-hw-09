25d7c3d11e11142ebd7300b5a54e5e66518b98e3
Julie Shapovalenko <jools.shapo@gmail.com>
2023-02-28 14:38:20 +0200

    update task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,3 @@
-
 const refs = {
     body: document.querySelector('body'),
     startBtn: document.querySelector('button[data-start]'),
@@ -12,22 +11,30 @@ refs.startBtn.addEventListener('click', onStartBtnClick);
 refs.stopBtn.addEventListener('click', onStopBtnClick);
 
 function onStartBtnClick() {
-    refs.body.style.backgroundColor = getRandomHexColor(); 
+    
+    refs.body.style.backgroundColor = getRandomHexColor();
 
-    if(intervalId) return;
+    refs.startBtn.setAttribute('disabled', '');
+    refs.stopBtn.removeAttribute('disabled');
     
     intervalId = setInterval(() => {
-        console.log('color')
-        refs.body.style.backgroundColor = getRandomHexColor()
+        refs.body.style.backgroundColor = getRandomHexColor();
+        console.log("start")
     }, PROMPT_DELAY);
-            
+
+    if (intervalId) return;
+      
+    
+}
+
+function onStopBtnClick() {
+    console.log('stop')
+
+    clearInterval(intervalId);
+    refs.stopBtn.setAttribute('disabled', '');
+    refs.startBtn.removeAttribute('disabled');
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 } 
-
-function onStopBtnClick() {
-    clearInterval(intervalId);
-    intervalId = 0;
-}
